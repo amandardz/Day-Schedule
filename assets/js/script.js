e92a44f5daddf3de0bf321d3b43fc5ea85d22952
Amanda Rodriguez <amandardz15@gmail.com>
2021-03-13 17:33:49 -0600

    modifying javascript

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,11 +6,11 @@ $('#currentDay').text(currentDate);
 var currentTime = moment().hour()
 console.log(typeof currentTime)
 
-//Creating an iteration with condition to compare times in order to change colors
+//Creating an iteration to loop through each time-block
 $('.time-block').each(function(){
     var hourBlock = parseInt($(this).attr('id'))
     console.log(typeof hourBlock)
-    
+    //Creating condition to compare time and change colors
     if(hourBlock < currentTime) {
         $(this).addClass('past')
         $(this).removeClass('present')
@@ -26,4 +26,20 @@ $('.time-block').each(function(){
     }
  });
 
- 
+//Creating local storage
+ $('.saveBtn').on('click', function(){
+    var timeBlockKey = $(this).parent().attr('id')
+    var textAreaVal = $(this).siblings('textarea').val() 
+    localStorage.setItem(timeBlockKey, textAreaVal)
+})
+
+//Retrieving from local storage
+$('#9 textarea').val(localStorage.getItem("9"))
+$('#10 textarea').val(localStorage.getItem("10"))
+$('#11 textarea').val(localStorage.getItem("11"))
+$('#12 textarea').val(localStorage.getItem("12"))
+$('#13 textarea').val(localStorage.getItem("13"))
+$('#14 textarea').val(localStorage.getItem("14"))
+$('#15 textarea').val(localStorage.getItem("15"))
+$('#16 textarea').val(localStorage.getItem("16"))
+$('#17 textarea').val(localStorage.getItem("17"))
